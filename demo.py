9f0e714b63328d98409e03cebe1262b4799e6e53
Hakan ÖZGÜR <hakan.ozgur@gmail.com>
2023-12-17 00:00:58 +0300

    component test

--- demo.py
+++ demo.py
@@ -3,10 +3,21 @@ from uix.elements import div, button
 
 def on_click(session, id, value):
     print("Clicked!", id, value, session.sid)
-    session.elements["btn_1"].value = "Clicked!" + str(session.next_id())
-    
+    session.elements[id].value = "Clicked!" + str(session.next_id())
+
+def comp1():
+    with div("A!",):
+        with div("C!"):
+            button("E!").on("click", on_click)
+
+    with div("B!"):
+        with div("D!"):
+            div("F!")
+
+
 with div("Hello World!") as main:
     with div("A!",):
+        comp1()
         with div("C!"):
             button("E!").on("click", on_click)
 
