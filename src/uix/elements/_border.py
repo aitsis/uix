8dcd25e8aca2008b178b0042cb68b08d60f10453
mugegngrd <muge@ait.com.tr>
2023-12-28 16:59:07 +0300

    Added descriptions and examples to existing elements

--- src/uix/elements/_border.py
+++ src/uix/elements/_border.py
@@ -4,3 +4,17 @@ class border(Element):
     def __init__(self,value,id = None):
         super().__init__(value, id = id)
         self.classes.append("border")
+
+title = "Border"
+
+description = '''
+Border elementi. Kenarında 1px kalınlığında çizgi bulunan bir div oluşturur. İçerisine elemanlar eklenerek kullanılır.
+id= Border elementinin id'si
+value= Border elementinin içeriği
+'''
+
+sample = """
+with border(''):
+    with div(''):
+        text("Border")    
+"""
