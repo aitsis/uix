8620e3c68c4019867201feaf779c009e63d9bca5
semiharslanait <semih@ait.com.tr>
2024-01-03 11:33:43 +0300

    refactor radio.py sample

--- src/uix/elements/_radio.py
+++ src/uix/elements/_radio.py
@@ -21,9 +21,20 @@ description = """
 
 
 sample = """
-with Form() as f:
-    Radio(name = "group1"):
-    Radio(name = "group1"):
-    Radio(name = "group1"):
+def radio_example():
+    with div().cls('row').style("width","fit-content").style("margin-bottom","20px"):
+        radio("Radio 1","Radio 1",name="radio1").on("change", on_change)
+        radio("Radio 2","Radio 2",name="radio1").on("change", on_change)
+        radio("Radio 3","Radio 3", name="radio1").on("change", on_change)
+        test = div("Test","Test")
+
+def on_change(ctx, id, value):
+    print("Changed", id, value)
+    if id == "Radio 4":
+        ctx.elements["Test"].value = "Peki"
+        ctx.elements["Test"].update()
+    else:
+        ctx.elements["Test"].value = id
+        ctx.elements["Test"].update()
 
 """
