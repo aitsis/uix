baebe10c2f9da3a7875665fdb6df82b56df68522
Emre <emre@ait.com.tr>
2024-01-03 14:24:49 +0300

    edited sample code in elements

--- src/uix/elements/_select.py
+++ src/uix/elements/_select.py
@@ -26,12 +26,11 @@ description = '''
 sample = """
 def on_change(ctx,id, value):
     ctx.elements["output"].value = value
-    ctx.elements["output"].update()
 
 def select_option_example():
     with select("Option 3", "mySelect").on("change",on_change):
         option("Option 1")
         option("Option 2").selected()
         option("Option 3")
-    div("", id="output")   
+    text("", id="output")         
 """
--- src/uix/elements/_slider.py
+++ src/uix/elements/_slider.py
@@ -33,10 +33,20 @@ description = """
 """
 sample = """
 
-    def on_slider_change():
-        print("Slider değeri değişti")
-        print("Yeni değer: ",slider1.value)
-    with parent:
-        slider(id="slider1",min=0,max=100,step=1).on("change",lambda: on_slider_change())
+def on_slider_change(ctx, id, value):
+    print("Slider değeri değişti")
+    ctx.elements[id].value = value
+    ctx.elements["myInput"].value = value
+    print("Yeni değer: ", ctx.elements[id].value)
+
+def input_changed(ctx, id, value):
+    ctx.elements[id].value = value
+    ctx.elements["mySlider"].value = value
+    print("Yeni değer: ", ctx.elements[id].value)
+
+def slider_example():
+    main = slider(id="mySlider", min=0, max=100, value=50, step="5").style("width", "400px").on("change", on_slider_change)
+    input(type="number", id="myInput", value=main.value).on("change", input_changed)
+    return main
         
 """
