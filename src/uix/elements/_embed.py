2c497a4be76ad9e1f81992ba2b9e3f30d40bde78
Mert <mert@ait.com.tr>
2024-01-11 10:20:59 +0300

    Update input element constructor to include step, min, and max attributes

---
+++ src/uix/elements/_embed.py
@@ -0,0 +1,44 @@
+from ..core.element import Element
+print("Imported: embed")
+
+class embed(Element):
+    def __init__(self, value=None, id=None, type=None, width=None, height=None):
+        super().__init__(value = value, id=id)
+        self.tag = "embed"
+        self.value_name = "src"
+
+        self.has_content = False
+
+        if type is not None:
+            self.attrs["type"] = type
+
+        if width is not None:
+            self.attrs["width"] = width
+
+        if height is not None:
+            self.attrs["height"] = height
+
+        
+
+
+title = "Embed"
+
+description = '''
+# embed(value,id,type,width,height)
+1. Embed elementi. Html'deki embed elementine karşılık gelir.
+    | attr          | desc                                                        |
+    | :------------ | :------------------------------------------------           |
+    | id            | Embed elementinin id'si                                     |
+    | value         | Embed elementinin içeriği                                   |
+    | type          | Embed elementinin tipi(video/webm, image/png, default: None!|             
+    | width         | Embed elementinin genişliği                                 |
+    | height        | Embed elementinin yüksekliği                                |
+'''
+
+sample = """
+from uix.elements import embed
+
+def embed_example():
+    embed("https://ai.ait.com.tr/wp-content/uploads/AI_main-video-with-prompt-.mp4", id="myEmbed", type="video/webm", width="300", height="200")
+    embed("https://aitools.ait.com.tr/AIT_AI_LOGO.png", type="image/png", width="200", height="100")
+"""
--- src/uix/elements/_input.py
+++ src/uix/elements/_input.py
@@ -1,7 +1,7 @@
 from ..core.element import Element
 print("Imported: input")
 class input(Element):
-    def __init__(self,value = None,id = None,type='text',name=None,placeholder="",required=False,list=None):
+    def __init__(self,value = None, id = None, type='text', name=None, placeholder="", required=False, list=None, step=None, min=None, max=None):
         super().__init__(value, id = id)
         self.tag = "input"
         self.value_name = "value"
@@ -21,6 +21,15 @@ class input(Element):
         
         if list is not None:
             self.attrs["list"] = list
+        
+        if step is not None:
+            self.attrs["step"] = step
+
+        if min is not None:
+            self.attrs["min"] = min
+        
+        if max is not None:
+            self.attrs["max"] = max
 
     def disabled(self):
         self.attrs["disabled"] = "disabled"
@@ -32,16 +41,18 @@ title = "Input"
 description = '''
 # input(value,id,type,name,placeholder,step,required)
 1. Temel input elementi.
-    | attr          | desc                                              |
-    | :------------ | :------------------------------------------------ |
-    | id            | Input elementinin id'si                          |
+    | attr          | desc                                            |
+    | :------------ | :-----------------------------------------------|
+    | id            | Input elementinin id'si                         |
     | value         | Input elementinin içeriği                       |
     | type          | Input elementinin tipi                          |
     | name          | Input elementinin name'i                        |
     | placeholder   | Input elementinin placeholder'i                 |
-    | step          | Input elementinin step'i                        |
     | required      | Input elementinin required'u                    |
     | list          | Input elementinin list'i                        |
+    | step          | Input elementinin step'i                        |
+    | min           | Input elementinin min'i                         |
+    | max           | Input elementinin max'i                         |
 '''
 sample = """
 def input_example():
