558bcf5208258fddaa954aa11629410c22dcf776
Hakan ÖZGÜR <hakan.ozgur@gmail.com>
2024-01-28 18:23:21 +0300

    image with PIL Image support

--- src/uix/elements/_image.py
+++ src/uix/elements/_image.py
@@ -1,12 +1,47 @@
+import uix
+from uuid import uuid4
 from ..core.element import Element
+from PIL import Image
+import io
 print("Imported: image")
 class image(Element):
-    def __init__(self,value:str = None,id:str = None):
-        super().__init__(value, id = id)
+    def __init__(self,value = None,id:str = None, no_cache = True):    
+        super().__init__(None, id = id)
+        self.no_cache = no_cache
+        self.value = value
         self.tag = "img"
         self.value_name = "src"
         self.has_content = False
+        
 
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        if isinstance(value, Image.Image):
+            self.has_png_image = True
+            self._value = self.create_image_url(value)
+        else:
+            self.has_png_image = False
+            self._value = value
+            
+        if self.session is not None:
+            self.send_value(self._value)
+
+    def __del__(self):
+        if self.has_png_image:
+            uix.app.files[self.id] = None
+
+    def create_image_url(self,img):
+        if self.id is None:
+            self.id = str(uuid4())
+        temp_data = io.BytesIO()
+        img.save(temp_data, format="png")
+        temp_data.seek(0)
+        uix.app.files[self.id] = {"data":temp_data.read(),"type":"image/png"}
+        return "download/"+self.id + "?" + str(uuid4()) if self.no_cache else "download/"+self.id
 title = "Image"
 
 description = '''
--- test.py
+++ test.py
@@ -1,14 +1,35 @@
 
 import uix
-from uix.elements import div, button, container, unorderedlist, listitem
+from uix.elements import container, image, button
+from uix_components import fabric
+from PIL import Image, ImageDraw
+import io
+img = Image.new("RGB",(300,300),"white")
 
-def on_mouse_down(ctx, id, value):
-    print("mouse down :",id,value)
+radius = 1
 
+def drawCircle(draw,x,y,r,color):
+    draw.ellipse((x-r,y-r,x+r,y+r),fill=color)
+
+
+def drawImage(img):
+    draw = ImageDraw.Draw(img)
+    draw.rectangle((0,0,300,300),fill="white")
+    drawCircle(draw,150,150,radius,"red")
+    
 
 def _root():
-    main = div().on("mousedown",on_mouse_down).size(300,300).on("keydown",lambda ctx,id,value: print("keydown",id,value)).cls("border").attr("tabindex",0)
+    with container(id="container1") as main:
+        image(id = "image1", value = img).size(None,100).on("click",update)
+        fabric(id="fabric1",width=300,height=300).bg("white")
     return main
 
+def update(ctx,id,value):
+    global radius
+    radius += 5
+    if radius > 150:
+        radius = 1
+    drawImage(img)
+    ctx.session.elements["image1"].value = img
 
 uix.start(ui = _root(), config = {"debug":True})
