74f8cf3f0eacaac7adb2d32214ed75f0f6fb6484
umutceliker <umut@ait.com.tr>
2024-01-29 16:58:21 +0300

    Table example sample ı iyileştirildi.

--- src/uix/elements/_table.py
+++ src/uix/elements/_table.py
@@ -16,20 +16,56 @@ description = '''
 | value         | Table elementinin içeriği                       |
 '''
 sample = """
+import uix
+from uix.elements import table, tr, td, tbody, th, thead, row # type: ignore
+from uix.elements._table import title, description, sample as code
+
+uix.html.add_css("custom-table",'''
+          
+#custom-table thead {    
+  background-color: #ff000026;
+} 
+
+#custom-table tr:nth-child(even){ 
+  background-color: black;
+}
+                 
+}
+''')
+
+
+fake_data = [DATA JSON BURAYA]
+
 def table_example():
-    with table("", id="table_example"):
-        global fake_data
-        with thead("",id="table_example_header"):
-            with tr("",id="table_example_header_row"):
-                th("ID")
-                th("Name")
-                th("Username")
-                th("Email")
-        with tbody("",id="table_example_body"):
-            for item in fake_data:
-                with tr("",id=item["id"]):
-                    td(item["id"])
-                    td(item["name"])
-                    td(item["username"])
-                    td(item["email"])
+    global fake_data
+    with row().style("width: max-content; gap: 10px;"):
+        with table("", id="table_example"):
+            with thead("",id="table_example_header"):
+                with tr("",id="table_example_header_row"):
+                    th("ID")
+                    th("Name")
+                    th("Username")
+                    th("Email")
+            with tbody("",id="table_example_body"):
+                for item in fake_data:
+                    with tr("",id=item["id"]):
+                        td(item["id"])
+                        td(item["name"])
+                        td(item["username"])
+                        td(item["email"])
+
+        with table("", id="custom-table"):
+            with thead(""):
+                with tr(""):
+                    th("ID")
+                    th("Name")
+                    th("Username")
+                    th("Email")
+            with tbody("",id="custom-table_body"):
+                for item in fake_data:
+                    with tr("",id="custom-"+str(item["id"])):
+                        td(item["id"])
+                        td(item["name"])
+                        td(item["username"])
+                        td(item["email"])
 """
