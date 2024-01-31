92be483c54246c8f9174bd9a56ce77c857ea18ae
Emre <emre@ait.com.tr>
2024-01-31 15:32:09 +0300

    file elementinin description ve sample code kısmı güncellendi

--- src/uix/elements/_file.py
+++ src/uix/elements/_file.py
@@ -113,25 +113,76 @@ description = """
 | :------------------- | :----------------------------------------------------------------- |
 | value                | Elementin içeriği.                                                 |
 | id                   | Elementin id'si                                                    |
-| multiple             | Birden fazla dosya seçilmesine izin verir.                         |
-| save_path            | Dosyanın kaydedileceği yol.                                        |
-| on_upload_done       | Dosya yüklendiğinde çalışacak fonksiyon.                           |
-| on_upload_started    | Dosya yüklenmeye başladığında çalışacak fonksiyon.                 |
-| on_error             | Dosya yüklenirken hata oluştuğunda çalışacak fonksiyon.            |
-| accept               | Seçilebilir  dosya uzantısı tanımlama (audio/* ,video/*, image/*)  |
-| useAPI               | Dosya yükleme işlemini API üzerinden yapar.                        |
+| multiple             | Birden fazla dosya seçilmesine izin verir.                          |
+| accept               | Seçilebilecek dosya tiplerini belirtir.                             |
+| callback             | Dosya seçildiğinde çağrılacak fonksiyon.                            |
 """
 sample = """
-from uix.elements import file, div, image, col
+import uix
+from uix.elements import file, div, image, col, row, progress, check, button, container
+from uix.elements._file import title, description, sample as code
 
-def file1():
-    with col().cls("border"):
-        file("",id="myFile", accept=".png, .jpg")
-        image("", id="img").style("margin-top","20px").style("width","300px").style("height","300px")
+from uix.core.file import File 
+def lineitem(value):
+    div(value).style("padding","5px;")
     
+def upload_file(ctx, id, value):
+    print("upload_file",id)
+    ctx.elements["file_123"].upload(id)
+    
+
+def line(file):
+    with row(id=file.url).style("margin-top","10px").on("click", upload_file) as r:
+        image(value=file.url).size(None,100)
+        lineitem(file.name)
+        lineitem(file.size)
+        lineitem(file.type)
+        progress(id = file.url+"_progress")
+            
+
+def list_files(ctx, files):
+    with ctx.elements["files"]:
+        for file in files:
+            line(file)
+    ctx.elements["files"].update()
+
+def show_error(ctx, error):
+    with ctx.elements["files"]:
+        div(error).style("color","red")
+    ctx.elements["files"].update()
+
+
+def select_callback(ctx, id, data, status):
+    if status == "done":
+        list_files(ctx, data)
+    else:
+        show_error(ctx, data)
+
+def upload_callback(ctx, id, data, status):
+    if status == "done":
+        print("upload done",len(data))
+        #>>>>>>>>>>>>>>>>>>>
+        # USE DATA HERE
+        #<<<<<<<<<<<<<<<<<<<<
+    elif status == "progress":
+        ctx.elements[data["url"] + "_progress"].value = data["progress"]
+    else:
+        show_error(ctx, data)
+
+def file_callback(ctx, id, event, data, status):
+    if event == "select":
+        select_callback(ctx, id, data, status)    
+    elif event == "upload":
+        upload_callback(ctx, id, data, status)
+
+
 def file_example():
-    with div("") as main:
-        file1()
-    return main
+    multiple = False
+    with container() as root:
+        with row(id="root").center():
+            check(multiple, id="multiple", disabled = False).on("change", lambda ctx, id, value: ctx.elements["file_123"].set_attr("multiple",value))
+            file(id="file_123", multiple=multiple,callback = file_callback, accept="image/png,image/jpeg,image/gif")
+        div("Files:", id="files").style("margin-top","10px")
+    return root
 
 """
