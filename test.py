566bb14649d05ef9e026ee92ec2105e1f14948bf
Hakan ÖZGÜR <hakan.ozgur@gmail.com>
2024-02-08 17:53:15 +0300

    test.py

--- test.py
+++ test.py
@@ -1,7 +1,46 @@
 
 import uix
-from uix.elements import container, image, button
+from uix.elements import container, image, button, text
 from uix.core.session import context
+
+def disable(context, id, value):
+    context.elements["my-button"].set_attr("disabled", True)
+
+def enable(context, id, value):
+    context.elements["my-button"].set_attr("disabled", False)
+
+
+def components():
+    if context.session.paths[1] == "view1":
+        return view1()
+    elif context.session.paths[1] == "view2":
+        return view2()
+    
+    with container() as main:
+        button("Click me Components", id="my-button")
+        text("Hello world")
+    return main
+
+
+
+def view1():
+    with container() as main:
+        button("Click me 1", id="my-button")
+        text(context.session.args.get("name", "No name"))
+        text(context.session.args.get("email", "No name"))
+
+        button("Disable", id="disable-btn").on("click", disable)
+        button("Enable", id="enable-btn").on("click", enable)
+    return main
+
+def view2():
+    with container() as main:
+        button("Click me 2", id="my-button")
+        text(context.session.args.get("name", "No name"))
+        button("Disable", id="disable-btn").on("click", disable)
+        button("Enable", id="enable-btn").on("click", enable)
+    return main
+
 
 def _root():
     with container() as main:
@@ -9,8 +48,12 @@ def _root():
             print("Session:", context.session.args)
             print("Paths:", context.session.paths)
             print("Cookies:", context.session.cookies)
-        image("https://via.placeholder.com/150")
-        button(f"Click me {context.session.args['user'][0]}", id="my-button")
+
+        paths = context.session.paths
+        print("Paths:", paths)
+        if len(paths) > 0:
+            if paths[0] == "components":
+                return components()
     return main
 
 uix.start(ui = _root, config = {"debug":True})
