4922229f7bde03035cd34024ed34b0996e11bd13
Hakan ÖZGÜR <hakan.ozgur@gmail.com>
2024-04-13 23:35:10 +0300

    little fix

--- src/uix/elements/_grid.py
+++ src/uix/elements/_grid.py
@@ -1,7 +1,7 @@
 from ..core.element import Element
 print("Imported: grid")
 class grid(Element):
-    def __init__(self,value: str,id:str = None, columns:str = None, rows:str = None):
+    def __init__(self,value:str = None, id:str = None, columns:str = None, rows:str = None):
         super().__init__(value, id = id)
         self.style("display", "grid")
         if columns is not None:
