9d903b07986b30cf9c39078374619adb0027adf9
Hakan ÖZGÜR <hakan.ozgur@gmail.com>
2024-05-01 17:19:31 +0300

    image bug fix on download link and refactoring

--- src/uix/elements/_image.py
+++ src/uix/elements/_image.py
@@ -7,13 +7,13 @@ import io
 print("Imported: image")
 class image(Element):
     def __init__(self,value = None,id:str = None, no_cache = True):    
-        super().__init__(value = value, id = id)
+        super().__init__(value = None, id = id)        
         self.no_cache = no_cache
         self.tag = "img"
         self.value_name = "src"
         self.has_content = False
-        self.has_png_image = False
-        
+        self.has_PIL_image = False
+        self.value = value
     @property
     def value(self):
         return self._value
@@ -21,10 +21,10 @@ class image(Element):
     @value.setter
     def value(self, value):
         if isinstance(value, Image.Image):
-            self.has_png_image = True
-            self._value = self.create_image_url(value)
+            self.has_PIL_image = True
+            self._value = self._create_image_url(value)
         else:
-            self.has_png_image = False
+            self.has_PIL_image = False
             self._value = value
             
         if self.id is not None:
@@ -33,17 +33,17 @@ class image(Element):
             print("No id for image")
     
     def __del__(self):
-        if self.has_png_image:
+        if self.has_PIL_image:
             uix.app.files[self.id] = None
 
-    def create_image_url(self,img):
+    def _create_image_url(self,img):
         if self.id is None:
             self.id = str(uuid4())
         temp_data = io.BytesIO()
         img.save(temp_data, format="png")
         temp_data.seek(0)
         uix.app.files[self.id] = {"data":temp_data.read(),"type":"image/png"}
-        return "download/"+self.id + "?" + str(uuid4()) if self.no_cache else "download/"+self.id
+        return "/download/"+self.id + "?" + str(uuid4()) if self.no_cache else "download/"+self.id
 title = "Image"
 
 description = '''
