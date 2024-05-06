2492992888903d694a505357f992026b80a5f3b0
Mert <mert@ait.com.tr>
2024-05-06 11:41:44 +0300

    select element description update

--- src/uix/elements/_select.py
+++ src/uix/elements/_select.py
@@ -19,19 +19,23 @@ description = '''
 | attr          | desc                                              |
 | :------------ | :------------------------------------------------ |
 | id            | Select elementinin id'si                          |
-| value         | Select elementinin içeriği                       |
-| selected      | Option değerlerinden hangisinin başlangıç değeri olucak tanımlar. |
-| disabled      | Selectin etkinliğini kapatır. |
+| value         | Option elementi seçildiğinde alınacak değer       |
+| text          | Option elementinin metni                          |
+| disabled      | Select içeriğinin seçilmesini engeller            |
+| selected      | Başlangıçta seçili olacak option elementi         |
 '''
 
 sample = """
+from uix.elements import select, option, text
+from uix.elements._select import title, description, sample as code
+
 def on_change(ctx,id, value):
     ctx.elements["output"].value = value
 
 def select_option_example():
-    with select("Option 3", "mySelect").on("change",on_change):
-        option("Option 1")
-        option("Option 2").selected()
-        option("Option 3")
-    text("", id="output")         
+    with select(id= "mySelect").on("change",on_change):
+        option(text="Option 1", value="1")
+        option(text="Option 2", value="2").selected()
+        option(text="Option 3", value="3").disabled()
+    text("Selected Value", id="output")      
 """
